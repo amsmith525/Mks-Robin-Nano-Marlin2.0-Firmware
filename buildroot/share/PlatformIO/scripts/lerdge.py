caffb436e9a09ed76751517e0fa25a8e2c970800
Sourcery AI <>
2023-03-22 22:02:03 +0000

    'Refactored by Sourcery'

--- buildroot/share/PlatformIO/scripts/lerdge.py
+++ buildroot/share/PlatformIO/scripts/lerdge.py
@@ -15,8 +15,7 @@ def encryptByte(byte):
     byte = 0xFF & ((byte << 6) | (byte >> 2))
     i = 0x58 + byte
     j = 0x05 + byte + (i >> 8)
-    byte = (0xF8 & i) | (0x07 & j)
-    return byte
+    return (0xF8 & i) | (0x07 & j)
 
 def encrypt_file(input, output_file, file_length):
     input_file = bytearray(input.read())
@@ -30,14 +29,13 @@ def encrypt_file(input, output_file, file_length):
 # Encrypt ${PROGNAME}.bin and save it with the name given in build.encrypt
 def encrypt(source, target, env):
     fwname = board.get("build.encrypt")
-    print("Encrypting %s to %s" % (target[0].path, fwname))
-    firmware = open(target[0].path, "rb")
-    renamed = open(target[0].dir.path + "/" + fwname, "wb")
-    length = os.path.getsize(target[0].path)
+    print(f"Encrypting {target[0].path} to {fwname}")
+    with open(target[0].path, "rb") as firmware:
+        renamed = open(f"{target[0].dir.path}/{fwname}", "wb")
+        length = os.path.getsize(target[0].path)
 
-    encrypt_file(firmware, renamed, length)
+        encrypt_file(firmware, renamed, length)
 
-    firmware.close()
     renamed.close()
 
 if 'encrypt' in board.get("build").keys():
